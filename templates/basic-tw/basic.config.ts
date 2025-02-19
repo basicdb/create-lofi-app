00a9442b7137da1815bd11391a6aa0552dd03194
Rashid Aziz <aziz@sophire.com>
2025-02-18 17:57:22 -0800

    template basic-tw - add vite-pwa

--- templates/basic-tw/basic.config.ts
+++ templates/basic-tw/basic.config.ts
@@ -1,3 +1,7 @@
+// basic.config.ts lets you define the schema for your database
+// after updating this file, you may need to restart the dev server
+// docs: https://docs.basic.tech/info/schema 
+
 export const schema = {
   project_id: '',
   version: 0,
